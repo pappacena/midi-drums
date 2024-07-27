8fa0ec38d79cac93b18bd484b76200b7d2a45e0e
Thiago F Pappacena <pappacena@gmail.com>
2024-07-27 14:42:02 -0300

    Minor adjustments and adding ollama

--- midi_drums/video/source.py
+++ midi_drums/video/source.py
@@ -1,4 +1,6 @@
 import time
+
+from PIL import Image
 import cv2
 
 
@@ -6,13 +8,24 @@ class ImageSource:
     def __init__(self, cap) -> None:
         self.cap = cap
         self.stop_on_null_frame = False
-        self.frame_wait_ms = 10
+        self.frame_wait_ms = 5
+        self.frames_as_pil = False
     
     def get_next_frame(self):
         ret, frame = self.cap.read()
         if not ret:
             return None
+        if self.frames_as_pil:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame = Image.fromarray(frame)
         return frame
+    
+    def set_frame_size(self, width: int, height: int) -> None:
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    
+    def set_fps(self, fps: int) -> None:
+        self.cap.set(cv2.CAP_PROP_FPS, fps)
 
     def __iter__(self):
         while True:
