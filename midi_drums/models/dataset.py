9cf48f39114ab884a287c54867dd8abb07d1dfe2
Thiago F Pappacena <pappacena@gmail.com>
2024-07-30 21:50:59 -0300

    Failed experiments with llava

--- midi_drums/models/dataset.py
+++ midi_drums/models/dataset.py
@@ -29,7 +29,7 @@ class DatasetManager:
         self.drumstick_positions = []
         self.ollama_client = ollama_client
         self.json_md_re = re.compile(r'.*```json\n(.+)\n```.*', re.DOTALL)
-    
+
     def _get_json_from_llm_text(self, text: str) -> dict:
         """
         Get json from text.
@@ -38,17 +38,45 @@ class DatasetManager:
         if match is None:
             return None
         try:
-            return json.loads(match.group(1))
+            data = json.loads(match.group(1))
         except json.JSONDecodeError:
             LOGGER.error(f"Could not parse json from text: {text}")
             return None
+        
+        if isinstance(data, dict):
+            # Sometimes, it comes as {"stick1": position, "stick2": position}
+            data = list(data.values())
+        
+        if not isinstance(data, list):
+            LOGGER.error(f"Expected list, got {type(data)}")
+            return None
+        
+        if not len(data):
+            LOGGER.error(f"Expected list of length 1 at least, got {len(data)}")
+            return None
+        
+        if isinstance(data[0], list):
+            # Convert lists to dicts
+            return [
+                dict(x1=x1, x2=x2, y1=y1, y2=y2)
+                for x1, x2, y1, y2 in data
+            ]
+    
+        for item in data:
+            if not all(i in item for i in ('x1', 'x2', 'y1', 'y2')):
+                LOGGER.error(f"Expected dict with keys x1, x2, y1, y2, got {data}")
+                return None
+        return data
+        
     
     def _collect_samples(self) -> list[Image]:
         """
         Collects samples from the source.
         """
         for frame in self.source:
-            time.sleep(random.random())
+            # Get 10% of the frames
+            if random.random() < 0.1:
+                continue
             self.samples.append(frame)
             if len(self.samples) >= self.n_samples:
                 break
@@ -69,8 +97,8 @@ class DatasetManager:
                         content=(
                             'Output only json list of 2 dicts with format '
                             '[{x1,y1,x2,y2}, {x1,y1,x2,y2}]. '
-                            'No explanation: give me the bounding-box '
-                            'position of both drum sticks'
+                            'No explanation: give me the position of the boxes surounding the drum sticks '
+                            'in the image, in percentage of image size.'
                         ),
                         images=[buff]
                     )
