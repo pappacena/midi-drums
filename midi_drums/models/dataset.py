6b0277a7f920bebddf0fd9d6e3c31da237e3ca67
Thiago F Pappacena <pappacena@gmail.com>
2024-07-30 22:31:42 -0300

    Detect sticks

--- midi_drums/models/dataset.py
+++ midi_drums/models/dataset.py
@@ -1,16 +1,15 @@
-from io import BytesIO
-import json
 import random
-import re
-import time
 import logging
 
+import torch
 from PIL import Image
-import ollama
+from transformers import Owlv2Processor, Owlv2ForObjectDetection
 
 from midi_drums.video.source import ImageSource, frame_to_pil_image
 
 LOGGER = logging.getLogger(__name__)
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class DatasetManager:
@@ -20,54 +19,12 @@ class DatasetManager:
     def __init__(
         self, 
         source: ImageSource, 
-        ollama_client: ollama.Client, 
         n_samples: int
     ) -> None:
         self.source = source
         self.n_samples = n_samples
         self.samples = []
-        self.drumstick_positions = []
-        self.ollama_client = ollama_client
-        self.json_md_re = re.compile(r'.*```json\n(.+)\n```.*', re.DOTALL)
-
-    def _get_json_from_llm_text(self, text: str) -> dict:
-        """
-        Get json from text.
-        """
-        match = self.json_md_re.match(text)
-        if match is None:
-            return None
-        try:
-            data = json.loads(match.group(1))
-        except json.JSONDecodeError:
-            LOGGER.error(f"Could not parse json from text: {text}")
-            return None
-        
-        if isinstance(data, dict):
-            # Sometimes, it comes as {"stick1": position, "stick2": position}
-            data = list(data.values())
-        
-        if not isinstance(data, list):
-            LOGGER.error(f"Expected list, got {type(data)}")
-            return None
-        
-        if not len(data):
-            LOGGER.error(f"Expected list of length 1 at least, got {len(data)}")
-            return None
-        
-        if isinstance(data[0], list):
-            # Convert lists to dicts
-            return [
-                dict(x1=x1, x2=x2, y1=y1, y2=y2)
-                for x1, x2, y1, y2 in data
-            ]
-    
-        for item in data:
-            if not all(i in item for i in ('x1', 'x2', 'y1', 'y2')):
-                LOGGER.error(f"Expected dict with keys x1, x2, y1, y2, got {data}")
-                return None
-        return data
-        
+        self.drumstick_positions = []        
     
     def _collect_samples(self) -> list[Image]:
         """
@@ -85,24 +42,32 @@ class DatasetManager:
         """
         Get drumstick position for each frame.
         """
+        processor = Owlv2Processor.from_pretrained("google/owlv2-base-patch16-ensemble")
+        model = Owlv2ForObjectDetection.from_pretrained(
+            "google/owlv2-base-patch16-ensemble",
+            device_map=DEVICE,
+            low_cpu_mem_usage=True,
+        )
+        
         for frame in self.samples:
-            buff = BytesIO()
-            frame_to_pil_image(frame).save(buff, format='PNG')
-            buff.seek(0)
-            resp = self.ollama_client.chat(
-                model='llava', 
-                messages=[
-                    dict(
-                        role='user', 
-                        content=(
-                            'Output only json list of 2 dicts with format '
-                            '[{x1,y1,x2,y2}, {x1,y1,x2,y2}]. '
-                            'No explanation: give me the position of the boxes surounding the drum sticks '
-                            'in the image, in percentage of image size.'
-                        ),
-                        images=[buff]
-                    )
-                ]
+            image = frame_to_pil_image(frame)
+            inputs = processor(text=[["drumstick"]], images=image, return_tensors="pt").to(DEVICE)
+            with torch.no_grad():
+                outputs = model(**inputs)
+            
+            target_sizes = torch.Tensor([image.size[::-1]])
+            results = processor.post_process_object_detection(
+                outputs=outputs, 
+                target_sizes=target_sizes,
+                threshold=0.1
             )
-            response = resp["message"]["content"]
-            self.drumstick_positions.append(self._get_json_from_llm_text(response))
+            i = 0
+            boxes, scores = results[i]["boxes"], results[i]["scores"]
+            result_boxes = []
+            for box, score in zip(boxes, scores):
+                box = [round(i, 2) for i in box.tolist()]
+                result_boxes.append(box)
+                LOGGER.info(
+                    f"Detected with confidence {round(score.item(), 3)} at location {box}"
+                )
+            self.drumstick_positions.append(result_boxes)
